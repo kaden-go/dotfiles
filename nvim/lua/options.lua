9179e32578e0c1879f957a40132959a621342ab9
Kevin Aden <kaden@fd46d684-bc0c-4b4d-8e18-abfb85777259.fritz.box>
2025-09-22 21:01:34 +0200

    new files

--- nvim/lua/options.lua
+++ nvim/lua/options.lua
@@ -1,6 +1,12 @@
-vim.opt.number = true
-vim.opt.cursorline = true 
-vim.opt.relativenumber = true 
-vim.opt.shiftwidth = 4
+local opt = vim.opt 
 
+opt.number = true
+opt.relativenumber = true 
 
+opt.cursorline = true -- highlight the cursor line 
+opt.shiftwidth = 2 -- 2 spaces for indent width 
+
+opt.ignorecase = true -- ignore case when searching 
+opt.smartcase = true -- if you include mixed case in your search, assumes you want case-sensitivity 
+
+opt.wrap = false -- disable line wrapping 
